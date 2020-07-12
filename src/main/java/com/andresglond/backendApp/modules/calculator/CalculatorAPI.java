9852c62fdee7fa3f1d2d2b59550924731439af29
nancho313 <nancho313@hotmail.com>
2020-07-11 20:48:46 -0500

    add initial structure for Calculator Exercise

---
+++ src/main/java/com/andresglond/backendApp/modules/calculator/CalculatorAPI.java
@@ -0,0 +1,20 @@
+package com.andresglond.backendApp.modules.calculator;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "calculator")
+public class CalculatorAPI {
+
+    @PostMapping
+    public ResponseEntity executeOperation(@RequestBody OperationDTO operationDTO){
+
+        String response = null; // <-- Utilice esta variable para luego mostrar el resultado
+
+        return ResponseEntity.ok(response);
+    }
+}
