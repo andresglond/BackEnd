1bb1b8e927f495ad014350a0cd1f311f00e086e8
Andres Guerrero <andresglond@gmail.com>
2020-07-14 11:27:41 -0500

    methods for manage string #1

---
+++ src/main/java/com/andresglond/backendApp/modules/string/ManageString1.java
@@ -0,0 +1,27 @@
+package com.andresglond.backendApp.modules.string;
+
+public class ManageString1 {
+
+    public int quantityCharacters(String string) {
+        int result;
+        return result = string.length();
+    }
+
+    public String convertToUppercase(String string) {
+        String result;
+
+        return result = string.toUpperCase();
+    }
+
+    public String convertToLowercase(String string) {
+        String result;
+
+        return result = string.toLowerCase();
+    }
+
+    public String replaceStringInText(String text, String word1, String word2) {
+        String result;
+
+        return result = text.replaceAll(word1, word2);
+    }
+}
